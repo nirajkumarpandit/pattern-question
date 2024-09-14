bf84716ebe7a34fcb5ef294a10c6ee4ff8233815
nirajkumarpandit <nirajkumarpandit45@gmail.com>
2024-09-14 23:18:58 +0530

    simple pattern

---
+++ simplepattern.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the star number :";
+    cin>>n;
+    for(int i=1;i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
